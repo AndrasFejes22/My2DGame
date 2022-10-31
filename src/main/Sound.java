73b962f7e7c6b6359d3627cbd078ce21da9b67d3
AFejes22 <afejes22@gmail.com>
2022-10-31 19:44:43 +0100

    Sound class altered

--- src/main/Sound.java
+++ src/main/Sound.java
@@ -1,6 +1,7 @@
 package main;
 
-import javax.sound.sampled.Clip;
+import javax.sound.sampled.*;
+import java.io.IOException;
 import java.net.URL;
 
 public class Sound {
@@ -14,5 +15,32 @@ public class Sound {
         soundUrl[2] = getClass().getResource("/sound/powerup.wav");
         soundUrl[3] = getClass().getResource("/sound/unlock.wav");
         soundUrl[4] = getClass().getResource("/sound/fanfare.wav");
+    }
+
+    public void setFile(int i){
+
+        try {
+            AudioInputStream ais = AudioSystem.getAudioInputStream(soundUrl[i]);
+            clip = AudioSystem.getClip();
+            clip.open(ais);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void play(){
+        clip.start();
+    }
+
+    public void loop(){
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void stop(){
+        clip.stop();
     }
 }
