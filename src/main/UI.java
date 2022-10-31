4b7f71476620a3f30b383bac89e452e3292a8df4
AFejes22 <afejes22@gmail.com>
2022-10-31 22:32:14 +0100

    UI class altered

--- src/main/UI.java
+++ src/main/UI.java
@@ -9,6 +9,7 @@ public class UI {
 
     GamePanel gp;
     Font arial_40;
+    Font arial_80B;
     BufferedImage keyImage;
     public boolean messageOn = false;
     public boolean gameFinished = false;
@@ -19,6 +20,7 @@ public class UI {
     public UI(GamePanel gp) {
         this.gp = gp;
         arial_40 = new Font("Arial", Font.PLAIN, 40);
+        arial_80B = new Font("Arial", Font.BOLD, 70);
         OBJ_Key key = new OBJ_Key();
         keyImage = key.image;
     }
@@ -42,11 +44,21 @@ public class UI {
 
             text = "You found the treasure!";
             textLenght = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-
             x = gp.screenWidth/2 - textLenght/2;
             y = gp.screenHeight/2 - (gp.tileSize * 3);
-
             g2.drawString(text, x, y);
+
+            //Congratulation:
+            g2.setFont(arial_80B);
+            g2.setColor(Color.yellow);
+            text = "Congratulations!";
+            textLenght = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+            x = gp.screenWidth/2 - textLenght/2;
+            y = gp.screenHeight/2 + (gp.tileSize * 2);
+            g2.drawString(text, x, y);
+
+            gp.gameThread = null;
+
 
         } else {
             g2.setFont(arial_40);
